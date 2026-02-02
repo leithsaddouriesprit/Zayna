f26de2667a12eb7a948af0836fd27c3a1bfaffad
Leith <saddouri.leith@esprit.tn>
2026-02-02 23:33:38 +0100

    JavaFX

---
+++ src/main/java/tn/esprit/workshop/utilis/MainFFX.java
@@ -0,0 +1,31 @@
+package tn.esprit.workshop.utilis;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainFFX extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/AjouterPersonne.fxml"));
+            Parent root = loader.load(); ///flowmain incor pain scroll pain i dont know what im using in builder
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
